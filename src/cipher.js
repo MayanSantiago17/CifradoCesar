4ff07dedea2a9720b0bbf465b164e93781caa788
MayanSantiago17 <mayansantiago1703@gmail.com>
2022-07-26 19:02:25 -0500

    de diseño a encode funcional

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,36 @@
 const cipher = {
-  // ...
-};
+ 
+  cifrar: function (offset, text){
+    
+    
+    
+    let result = "";
+    for (let i =0; i < text.length; i++) {
+      let newText = (text.charCodeAt(i));
+      if (newText >= 65 && newText <= 90){
+        let inputText = (newText - 65 + offset) % 26 + 65;
+        let inputNewText = String.fromCharCode(inputText);
+        result += inputNewText;
+      }
+
+      else if (newText >= 97 && newText <= 122){
+        let inputText = (newText - 97 + offset) % 26 + 97;
+        let inputNewText = String.fromCharCode(inputText);
+        result += inputNewText; 
+        }
+      
+        else { 
+          result += String.fromCharCode(newText); 
+         
+        }
+      }
+
+    return result;
+    }
+
+
+
+}
+  
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+window.addEventListener("load", cesar, true);
+
+function cesar(){
+    /*document.getElementById("msjpcifrar").addEventListener("keyup", function(){
+        this.value = this.value.toUpperCase();
+    }, true*/
+
+    document.getElementById("cifrar").addEventListener("click",function(){
+        let text = document.getElementById("msjpcifrar").value;
+        let offset = document.getElementById("dropdown123").value;
+        document.getElementById("msjpdescifrar").value = cipher.cifrar(offset, text);
+    }, true)
+
+    /*document.getElementById("descifrar").addEventListener("click",function(){
+        let text = document.getElementById("msjpcifrar").value;
+        let offset = document.getElementById("dropdown123").value;
+        document.getElementById("msjpdescifrar").value = cipher.descifrar(offset, text);
+    }, true);*/
+
+    
+
+}
+
+
+
+
+
+
+
+
+
